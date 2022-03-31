6fed4b19d652d9af91114370fe79d23640b7a012
christian julius guillen <juliusguillen@gmail.com>
2022-03-31 08:10:55 -0500

    commit tree 9a175e257cfaf5a0199a689c782718896a0ad0b9

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const id = document.querySelector('p')
+    id.textContent = "This is really cool!";
+  });
+    
